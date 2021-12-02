7f50d6b89e35f5baa728062bd04b10edc9283eee
brinleymiller122 <miller.brinley@gmail.com>
2021-12-02 11:22:38 -0700

    make search inputs

--- drug/models.py
+++ drug/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+# Create your models here.
+
+class Drugs(models.Model):
+    #drugid=models.IntegerField()
+    drugname = models.CharField(max_length=30)
+    isopioid = models.CharField(max_length=5)
+   
+    class Meta:
+        db_table= 'pd_drugs'
+    
+    def __str__(self):
+        return (self.drugname)  #may need fixed
+
+class Prescriber(models.Model):
+    npi = models.IntegerField(unique=True)
+    fname = models.CharField(max_length=11)
+    lname = models.CharField(max_length=11)
+    gender = models.CharField(max_length=1)
+    state = models.CharField(max_length=2)
+    specialty = models.CharField(max_length=62)
+    isopioidprescriber = models.CharField(max_length=5)
+    totalprescriptions = models.IntegerField()
+
+    class Meta:
+        db_table= 'pd_prescriber'
+    
+    def __str__(self):
+        return (self.fname) + " " + (self.lname)  #may need fixed
+
+class Credentials(models.Model):
+    npi = models.IntegerField() #nned to be primary or foriegn?
+    credentials = models.CharField(max_length=20)
+    
+    
+    class Meta:
+        db_table= 'pd_credentials'
+    
+    def __str__(self):
+        return (self.credentials)  #may need fixed
+
+class StateData(models.Model):
+    stateabbrev = models.CharField(max_length=2) #may need to be primary key
+    population = models.IntegerField()
+    deaths = models.IntegerField()
+    
+    class Meta:
+        db_table= 'pd_statedata'
+    
+    def __str__(self):
+        return (self.stateabbrev)  #may need fixed
+
+class StateAbbreviation(models.Model):
+    stateabbrev = models.CharField(max_length=2)
+    state = models.CharField(max_length=14) # may need to be primary and foreign key
+    
+
+    class Meta:
+        db_table= 'pd_stateabbreviation'
+    
+    def __str__(self):
+        return (self.state)  #may need fixed
+
+class Triple(models.Model):
+    id = models.IntegerField(primary_key=True) #may need to be removed-primary key
+    prescriberid = models.ForeignKey(Prescriber, default="", on_delete= models.DO_NOTHING, to_field='npi')
+    drugname = models.CharField(max_length=30)
+    qty = models.IntegerField()
+    
+    
+    class Meta:
+        db_table= 'pd_triple'
+    
+    def __str__(self):
+        return (self.drugname) #may need fixed
--- drug/views.py
+++ drug/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Prescriber
+from .models import StateData
+from .models import StateAbbreviation
+from .models import Credentials
+from .models import Drugs
+from .models import Triple
 
 # Create your views here.
 
@@ -22,4 +28,14 @@ def prescriberDetailPageView(request):
     return render(request, 'drug/prescriberDetail.html')
 
 def prescriberSearchPageView(request):
+    if request.method == "POST":
+        prescriber = Prescriber.objects.all() #maybe should be get() id or something
+
+        prescriber.fname= request.POST['fname']
+        prescriber.lname= request.POST['lname']
+        prescriber.credentials= request.POST['credentials'] 
+        prescriber.gender= request.POST['gender'] 
+        prescriber.state= request.POST['state'] 
+        prescriber.specialty= request.POST['specialty'] 
+    
     return render(request, 'drug/prescriberSearch.html')
