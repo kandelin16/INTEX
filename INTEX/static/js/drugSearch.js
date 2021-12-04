fa8c7e59a1bc4bea0dd5dc3f5fd12717a37b7d15
brinleymiller122 <miller.brinley@gmail.com>
2021-12-04 09:14:49 -0700

    drug search search features added

--- INTEX/static/js/drugSearch.js
+++ INTEX/static/js/drugSearch.js
@@ -1,3 +1,38 @@
+function filterGender(drs) {
+  input = document.getElementById("filterByIsOpioid").value.toLowerCase();
+  if (input != "") {
+    for (var count = 0; count < drs.length; count++) {
+      tempDoc = drs[count];
+      genObject = document.getElementById(tempDoc.id + "gender");
+      if (genObject.innerHTML.toLowerCase().includes(input)) {
+        tempDoc.hidden = false;
+      } else {
+        tempDoc.hidden = true;
+      }
+    }
+  }
+}
+
+function filterState(drs) {
+  input = document.getElementById("filterByState").value.toLowerCase();
+  passDown = [];
+  if (input != "") {
+    for (var count = 0; count < drs.length; count++) {
+      tempDoc = drs[count];
+      genObject = document.getElementById(tempDoc.id + "state");
+      if (genObject.innerHTML.toLowerCase().includes(input)) {
+        tempDoc.hidden = false;
+        passDown.push(tempDoc);
+      } else {
+        tempDoc.hidden = true;
+      }
+    }
+    filterGender(passDown);
+  } else {
+    filterGender(drs);
+  }
+}
+
 function filterDrug() {
   dru = document.getElementsByClassName("drugclass");
   input = document.getElementById("filterTable").value.toLowerCase();
--- INTEX/static/js/prescriberSearch.js
+++ INTEX/static/js/prescriberSearch.js
@@ -1,61 +1,59 @@
-function filterGender(drs) {
-    input = document.getElementById("filterByGender").value.toLowerCase()
-    if (input != "") {
-        for (var count = 0; count < drs.length; count++) {
-            tempDoc = drs[count]
-            genObject = document.getElementById(tempDoc.id + "gender")
-            if (genObject.innerHTML.toLowerCase().includes(input)) {
-                tempDoc.hidden = false
-            }
-            else {
-                tempDoc.hidden = true
-            }
-        }
+function filterOpioid(dru) {
+  input = document.getElementById("filterIsOpioid").value.toLowerCase();
+  if (input != "") {
+    for (var count = 0; count < dru.length; count++) {
+      tempDrug = dru[count];
+      genObject = document.getElementById(tempDoc.id + "isopiod");
+      if (genObject.innerHTML.toLowerCase().includes(input)) {
+        tempDrug.hidden = false;
+      } else {
+        tempDrug.hidden = true;
+      }
     }
+  }
 }
 
-function filterState(drs) {
-    input = document.getElementById("filterByState").value.toLowerCase()
-    passDown = []
-    if (input != "") {
-        for (var count = 0; count < drs.length; count++) {
-            tempDoc = drs[count]
-            genObject = document.getElementById(tempDoc.id + "state")
-            if (genObject.innerHTML.toLowerCase().includes(input)) {
-                tempDoc.hidden = false
-                passDown.push(tempDoc)
-            }
-            else {
-                tempDoc.hidden = true
-            }
-        }
-        filterGender(passDown)
+function filterDrugName(drs) {
+  input = document.getElementById("filterByState").value.toLowerCase();
+  passDown = [];
+  if (input != "") {
+    for (var count = 0; count < drs.length; count++) {
+      tempDoc = drs[count];
+      genObject = document.getElementById(tempDoc.id + "state");
+      if (genObject.innerHTML.toLowerCase().includes(input)) {
+        tempDoc.hidden = false;
+        passDown.push(tempDoc);
+      } else {
+        tempDoc.hidden = true;
+      }
     }
-    else {
-        filterGender(drs)
-    }
+    filterGender(passDown);
+  } else {
+    filterGender(drs);
+  }
 }
 
 function filter() {
-    drs = document.getElementsByClassName("drVis")
-    input = document.getElementById("filterTable").value.toLowerCase()
-    passDown = []
-    if (input != "") {
-        for (var count = 0; count < drs.length; count++) {
-            tempDoc = drs[count]
-            nameObject = document.getElementById(tempDoc.id + "name")
-            specObject = document.getElementById(tempDoc.id + "spec")
-            if (nameObject.innerHTML.toLowerCase().includes(input) || specObject.innerHTML.toLowerCase().includes(input)) {
-                tempDoc.hidden = false
-                passDown.push(tempDoc)
-            }
-            else {
-                tempDoc.hidden = true
-            }
-        }
-        filterState(passDown)
+  drs = document.getElementsByClassName("drVis");
+  input = document.getElementById("filterTable").value.toLowerCase();
+  passDown = [];
+  if (input != "") {
+    for (var count = 0; count < drs.length; count++) {
+      tempDoc = drs[count];
+      nameObject = document.getElementById(tempDoc.id + "name");
+      specObject = document.getElementById(tempDoc.id + "spec");
+      if (
+        nameObject.innerHTML.toLowerCase().includes(input) ||
+        specObject.innerHTML.toLowerCase().includes(input)
+      ) {
+        tempDoc.hidden = false;
+        passDown.push(tempDoc);
+      } else {
+        tempDoc.hidden = true;
+      }
     }
-    else {
-        filterState(drs)
-    }
+    filterState(passDown);
+  } else {
+    filterState(drs);
+  }
 }
