74bad959afd61646d01ca0bcc704f02d0097bf06
brinleymiller122 <miller.brinley@gmail.com>
2021-12-04 12:43:30 -0700

    attempt to show top10 prescribers

--- drug/views.py
+++ drug/views.py
@@ -2,7 +2,7 @@ from typing import ContextManager
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
-
+import re 
 
 # Create your views here.
 
@@ -14,9 +14,14 @@ def aboutPageView(request):
 
 def drugDetailPageView(request, drug):
     dru = models.Drug.objects.get(drugname=drug)
+    drug = re.sub(r'[^a-zA-Z]','', drug)
+    drug=drug.lower()
+    list= models.Prescriber.objects.order_by(drug)[10]
+    print(list)
 
     context = {
         'dru': dru,
+        "list": list
         
     }
     return render(request, 'drug/drugDetail.html', context)
